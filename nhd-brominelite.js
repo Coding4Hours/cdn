3427c259d51285665f4f294ed6d5825495bd7978
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-11-27 15:51:21 -0800

    Update nhd-brominelite.js

--- nhd-brominelite.js
+++ nhd-brominelite.js
@@ -11,25 +11,32 @@
                                                                                                                                                                                                                                               
 replace file with the file you want all_i_one.html, games.html, or proxy.html
 */
-  
+
   const file = "games.html"
 
-      try {
-        fetch(`https://cdn.jsdelivr.net/gh/Bromine-labs/BromineLite@main/${file}?t=`+Date.now())
+
+  document.addEventListener("keydown", function(event) {
+      if (event.key === "r") {
+          document.open();
+  try {
+      fetch(`https://cdn.jsdelivr.net/gh/Bromine-labs/BromineLite@main/${file}?t=` + Date.now())
           .then(response => response.text())
           .then(html => {
-                document.documentElement.innerHTML = html;
+              document.write(html);
 
-                document.documentElement.querySelectorAll('script').forEach(oldScript => {
-                    const newScript = document.createElement('script');
-                    if (oldScript.src) {
-                        newScript.src = oldScript.src;
-                    } else {
-                        newScript.textContent = oldScript.textContent;
-                    }
-                    document.body.appendChild(newScript);
-                });
+              document.querySelectorAll('script').forEach(oldScript => {
+                  const newScript = document.createElement('script');
+                  if (oldScript.src) {
+                      newScript.src = oldScript.src;
+                  } else {
+                      newScript.textContent = oldScript.textContent;
+                  }
+                  document.body.appendChild(newScript);
+              });
           });
-      } catch (error) {
-        console.error('error:', error);
+  } catch (error) {
+      console.error('error:', error);
+  }
+        document.close();
       }
+  });
